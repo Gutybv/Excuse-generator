6ca5c9cb91bef0d968903fc1944b8046c08fc0e9
Benjamin Gutierrez <gutybv@gmail.com>
2022-06-11 03:54:53 +0000

    finish

--- src/app.js
+++ src/app.js
@@ -5,7 +5,28 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+function randomExcuse(ran) {
+  return ran[Math.floor(Math.random() * ran.length)];
+}
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("p").innerHTML =
+    randomExcuse(who) +
+    " " +
+    randomExcuse(action) +
+    " " +
+    randomExcuse(what) +
+    " " +
+    randomExcuse(when);
 };
